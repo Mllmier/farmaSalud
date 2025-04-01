c97ad94207b9fedd3dc8db6c56204ddc94d6f68d
haiderelprogramador <haider32108@gmail.com>
2025-04-01 16:56:45 -0500

    feat: add class dao

--- src/farmasalud/view/login_.java
+++ src/farmasalud/view/login_.java
@@ -152,7 +152,7 @@ public class login_ extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
-        // TODO add your handling code here:
+        
     }//GEN-LAST:event_jTextField1ActionPerformed
 
     private void jPasswordField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jPasswordField1ActionPerformed
--- src/model/administrador.java
+++ src/model/administrador.java
@@ -1,13 +1,49 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package model;
 
-/**
- *
- * @author usuario
- */
 public class administrador {
-    
+   private String nombre;
+   private String apellido;
+   private String cedula;
+   private rol rol;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public rol getRol() {
+        return rol;
+    }
+
+    public void setRol(rol rol) {
+        this.rol = rol;
+    }
+
+    public administrador(String nombre, String apellido, String cedula, rol rol) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.cedula = cedula;
+        this.rol = rol;
+    }
+   
 }
