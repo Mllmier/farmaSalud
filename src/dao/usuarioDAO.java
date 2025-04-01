88c4209d9332cae429197922e8405c6de3cb98b5
Maria liz León <marializleonmier.com>
2025-04-01 16:58:30 -0500

    feat: dao para authenticacion como admn con user predentermado

---
+++ src/dao/usuarioDAO.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import model.rol;
+import model.usuario;
+
+/**
+ *
+ * @author Maria liz
+ */
+public class usuarioDAO {
+    
+    //admin creado con email y password
+    private static final usuario usuario_predeterminado = new usuario("maria@gmail.com ", "karen123", rol.ADMINISTRADOR);
+    
+    public usuario validarCredenciales(String email, String password) {
+        return usuario_predeterminado;
+    }
+    
+}
